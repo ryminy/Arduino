b6a12779f48105a1c133afd6bde8c6df1a513cdf
ryminy <mihail@linuxmint.(none)>
2016-04-14 22:15:20 +0300

    unsigned char -> char

--- Arduino.ino
+++ Arduino.ino
@@ -1,15 +1,33 @@
-#define MAXSIZE 6
-typedef struct _pkt
+#define MAXSIZE 7
+typedef struct _packet
 {
-  unsigned char src;
-  unsigned char dest;
-  unsigned char type;
-  unsigned char data[2];
-  unsigned char crc;
-} pkt;
+  char src;
+  char dest;
+  char type;
+  char data[2];
+  char crc;
+} packet;
 
-#define STRINGTOPACKET(str,pkt) \
-  ()
+void stringToPacket(packet * pkt, char *str)
+{
+   pkt->src = str[0];
+   pkt->dest = str[1];
+   pkt->type = str[2];
+   pkt->data[0] = str[3];
+   pkt->data[2] = str[4];
+   pkt->crc= str[5];
+}
+
+void packetToString(char *str, packet *pkt)
+{
+   str[0] = pkt->src;
+   str[1] = pkt->dest;
+   str[2] = pkt->type;
+   str[3] = pkt->data[0];
+   str[4] = pkt->data[2];
+   str[5] = pkt->crc;
+   str[6] = '\0';
+}
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -22,8 +40,9 @@ void setup() {
 
 void loop() {
   
-  char input[MAXSIZE+1];
+  char input[MAXSIZE];
   int i;
+  packet pkt;
 
   while(1)
   {
@@ -32,9 +51,9 @@ void loop() {
     
     while (Serial.available() > 0)
     {
-        if(i < MAXSIZE)
+        if(i < MAXSIZE-1)
         {
-          delay(3);
+          delay(2);
           input[i++] = Serial.read();
           input[i] = '\0';
         }
@@ -45,7 +64,7 @@ void loop() {
     if( input[0] != '\0')
     {
       Serial.print("Am primit stringul:");
-      Serial.print(input);
+      Serial.println(input);
     }  
   }
  
