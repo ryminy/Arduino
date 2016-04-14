c634214952a17895e3d57bbd1e516c4e28a7b194
ryminy <mihail@linuxmint.(none)>
2016-04-14 21:36:19 +0300

    while(1)

--- Arduino.ino
+++ Arduino.ino
@@ -1,4 +1,16 @@
-#define MAXSIZE 10
+#define MAXSIZE 6
+typedef struct _pkt
+{
+  unsigned char src;
+  unsigned char dest;
+  unsigned char type;
+  unsigned char data[2];
+  unsigned char crc;
+} pkt;
+
+#define STRINGTOPACKET(str,pkt) \
+  ()
+
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
@@ -12,24 +24,29 @@ void loop() {
   
   char input[MAXSIZE+1];
   int i;
-  i = 0;
-  input[0] = '\0';
-  
-  while (Serial.available() > 0)
+
+  while(1)
   {
-    if(i < 10)
+    i = 0;
+    input[0] = '\0';
+    
+    while (Serial.available() > 0)
     {
-      delay(2);
-      input[i++] = Serial.read();
-      input[i] = '\0';
-    }
-    else
-      break;
+        if(i < MAXSIZE)
+        {
+          delay(3);
+          input[i++] = Serial.read();
+          input[i] = '\0';
+        }
+        else
+          break;
+     }
+    
+    if( input[0] != '\0')
+    {
+      Serial.print("Am primit stringul:");
+      Serial.print(input);
+    }  
   }
-  
-  if( input[0] != '\0')
-  {
-    Serial.print("Am primit stringul:");
-    Serial.println(input);
-  }
+ 
 } 
