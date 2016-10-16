86ad7b354e0b86af7d5ee179b51d980c09649bab
ryminy <mihail.ocneanu@gmail>
2016-10-16 21:40:48 +0300

    Receive commands from Python

--- Arduino.ino
+++ Arduino.ino
@@ -1,28 +1,62 @@
+/*
+*Message format:
+* # # #  A P P P P 
+* A = action 
+* P =pin
+*/
+
 void setup()
 {
 Serial.begin(9600);
 pinMode(13,OUTPUT);
-digitalWrite(13,LOW);
+}
+
+void decodeMessage(int *pin, int*action, char data)
+{
+  if(data & 0x10)
+  {
+    *action = 1;
+  }
+  *pin = data & 0xf;
+}
+
+void setAction(int pin, int action)
+{
+  int i;
+  if (action == 1)
+  {
+    for( i = 0; i<10; i++)
+    {
+      digitalWrite(pin,HIGH);
+      delay(300);
+      digitalWrite(pin,LOW);
+      delay(300);
+    } 
+  }      
 }
 
 void loop()
 {
+  char inByte = ' ';
+  Serial.println("R");
   
-char inByte = ' ';
-Serial.println("R");
+  while(1)
+  {
+    if(Serial.available())
+    { 
+      inByte = Serial.read();
+    }
+    
+    if( inByte != ' ')
+    {
+        int pin =0;
+        int action = 0;
+        decodeMessage(&pin,&action,inByte);   
+        Serial.println(inByte);
 
-while(1)
-{
-  if(Serial.available())
-  { 
-    inByte = Serial.read();
-  }
+        setAction(pin, action);
+    }
   
-  if( inByte != ' ')
-  {   
-      Serial.println(inByte);
+    inByte = ' ';
   }
-
-  inByte = ' ';
-}
 }
